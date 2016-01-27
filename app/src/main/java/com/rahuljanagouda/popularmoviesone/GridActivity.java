98c97debc0cd5a6f41939c13cc0295f4890e9806
rahuljanagouda <rahul.janagouda@gmail.com>
2016-01-28 00:10:32 +0530

    feat : 1) added the bare bone of details screen 2) Changed the icon

--- app/src/main/java/com/rahuljanagouda/popularmoviesone/GridActivity.java
+++ app/src/main/java/com/rahuljanagouda/popularmoviesone/GridActivity.java
@@ -1,13 +1,14 @@
 package com.rahuljanagouda.popularmoviesone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class GridActivity extends AppCompatActivity {
 
@@ -22,6 +23,7 @@ public class GridActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), DetailsActivity.class));
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
